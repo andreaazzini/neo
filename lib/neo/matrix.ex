8ee521691697a46c41997c4b834f15ba4fcf4930
Andrea Azzini <and.azzini@gmail.com>
2015-10-29 14:50:14 +0100

    Remove Dict usage

--- lib/neo/matrix.ex
+++ lib/neo/matrix.ex
@@ -1,45 +1,36 @@
 defmodule Neo.Matrix do
-  use Dict
-
   defstruct [:map, :rows, :cols]
 
   def delete(%__MODULE__{rows: rows, cols: cols}, {row, col})
-  when row >= rows or col >= cols do
-    :error
-  end
+  when row >= rows or col >= cols, do: :error
   def delete(%__MODULE__{map: map} = matrix, {row, col}) do
     %{matrix | map: Map.delete(map, {row, col})}
   end
 
-  def fetch(%__MODULE__{rows: rows, cols: cols}, {row, col})
-  when row >= rows or col >= cols do
-    :error
+  def get(%__MODULE__{rows: rows, cols: cols}, {row, col})
+  when row >= rows or col >= cols, do: :error
+  def get(%__MODULE__{map: map}, {row, col}) do
+    case Map.fetch(map, {row, col}) do
+      {:ok, val} -> val
+      :error -> nil
+    end
   end
+
+  def fetch(%__MODULE__{rows: rows, cols: cols}, {row, col})
+  when row >= rows or col >= cols, do: :error
   def fetch(%__MODULE__{map: map}, {row, col}) do
     Map.fetch(map, {row, col})
   end
 
-  def new() do
-    %__MODULE__{map: %{}, rows: 0, cols: 0}
-  end
+  def new(), do: %__MODULE__{map: %{}, rows: 0, cols: 0}
 
-  def new(rows, cols) do
-    %__MODULE__{map: %{}, rows: rows, cols: cols}
-  end
+  def new(rows, cols), do: %__MODULE__{map: %{}, rows: rows, cols: cols}
 
   def put(%__MODULE__{rows: rows, cols: cols}, {row, col}, _)
-  when row >= rows or col >= cols do
-    :error
-  end
+  when row >= rows or col >= cols, do: :error
   def put(%__MODULE__{map: map} = matrix, {row, col}, val) do
     %{matrix | map: Map.put(map, {row, col}, val)}
   end
 
-  def reduce(%__MODULE__{map: map}, acc, fun) do
-    Enum.reduce(map, acc, fun)
-  end
-
-  def size(%__MODULE__{rows: rows, cols: cols}) do
-    {rows, cols}
-  end
+  def size(%__MODULE__{rows: rows, cols: cols}), do: {rows, cols}
 end
